bede1bdeded4a0983fbd228212a990ad5d0cd305
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-08-05 23:52:37 +0530

    Update hello_world.jl

--- diff_cal/hello_world.jl
+++ diff_cal/hello_world.jl
@@ -11,3 +11,5 @@ rhs(x) = x.rhs        # convenience to extract right hand side of an equation
 eq = rhs(out)    # just the right hand side
 C1 = first(setdiff(free_symbols(eq), (x,a))) # fish out constant
 c1 = solve(eq(x=>0) - 1//2, C1)
+
+eq(C1 => c1[1])
