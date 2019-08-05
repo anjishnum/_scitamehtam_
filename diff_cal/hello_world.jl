b695ebd5fa354151e959eb21cac36085bd315809
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2019-08-05 23:52:22 +0530

    Update hello_world.jl

--- diff_cal/hello_world.jl
+++ diff_cal/hello_world.jl
@@ -6,3 +6,8 @@ u = SymFunction("u")
 
 eqn = u'(x) - a * u(x) * (1 - u(x))
 out = dsolve(eqn)
+
+rhs(x) = x.rhs        # convenience to extract right hand side of an equation
+eq = rhs(out)    # just the right hand side
+C1 = first(setdiff(free_symbols(eq), (x,a))) # fish out constant
+c1 = solve(eq(x=>0) - 1//2, C1)
